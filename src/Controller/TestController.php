6a1e5934e996c51f857f09d01e2ec9bcdb04a4bf
Aymeric SIMORRE <aymericsimorre@gmail.com>
2020-12-04 12:22:58 +0100

    ajt controleur et twig

---
+++ src/Controller/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/",name="index")
+     *  */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/test",name="test")
+     *  */
+    public function test()
+    {
+        $tab= ["Aymeric", "Zoé", "Charles", "Amandine"];
+        return $this->render('test.html.twig', ['tab' => $tab] );
+    }
+}
