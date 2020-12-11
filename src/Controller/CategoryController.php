a1493c44489ff5acf524ec8d8c1abfdd088376b2
Aymeric SIMORRE <aymericsimorre@gmail.com>
2020-12-11 11:28:54 +0100

    Finalisation CRUD

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryFormType;
+use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,22 +27,62 @@ class CategoryController extends AbstractController
     }
 
     /**
+     * @Route("/categoryProduct/{id}", name="categoryProduct")
+     */
+    public function productByCategory(ProductRepository $productRepository, $id): Response
+    {
+        $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+        $listeProduit = $productRepository->findBy(['Category' => $category]);
+        
+        return $this->render('category/categoryProducts.html.twig', [
+            'listeProduit' => $listeProduit
+        ]);
+    }
+
+    /**
      * @Route("category/add", name= "ajoutCategorie")
      *
      * @return void
      */
-    public function addCategory(){
-        $em = $this->getDoctrine()->getManager();
-        
+    public function addCategory(Request $request, EntityManagerInterface $em){
+       
         $category = new Category;
-        $category
-        ->setName('Xiaomi')
-        ->setDescription('Un autre fabricant de smartphones')
-        ->setSlug('xiaomi');
+        $form= $this->createForm(CategoryType::class, $category);
 
-        $em->persist($category);
-        $em->flush();
-        dd('fait');
-        return $this->render('success.html.twig', []);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('success');
+        }
+
+        return $this->render('category/add.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("category/edit/{id}", name= "editCategory")
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param idCategory $id
+     * @return void
+     */
+    public function editCategory(Request $request, EntityManagerInterface $em, $id){
+
+        $category = $em->getRepository(Category::class)->find($id);
+        $form= $this->createForm(CategoryFormType::class, $category);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('success');
+        }
+
+        return $this->render('category/edit.html.twig', ['form' => $form->createView()]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,21 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Product;
-
+use App\Entity\Category;
+use App\Form\ProductFormType;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
+
     /**
      * @Route("/product/add", name="ajoutProduit")
      */
@@ -61,8 +64,63 @@ class ProductController extends AbstractController
             return $this->redirectToRoute('success', []);
         }
 
-        return $this->render('product/index.html.twig', [
+        return $this->render('product/add.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/product/{id}", name = "detailProduct")
+     *
+     * @param integer $id idCategory
+     * @return Response
+     */
+    public function showProduct(ProductRepository $productRepository, $id): Response
+    {
+        $product = $productRepository->find($id);
+        return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/product/edit/{id}", name= "editProduct")
+     *
+     * @param ProductRepository $productRepository
+     * @param integer $id idProduct
+     * @return Response
+     */
+    public function editProduct(Request $request, EntityManagerInterface $em, $id): Response
+    {
+        $product = $em->getRepository(Product::class)->find($id);
+
+        $form = $this->createForm(ProductFormType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('success');
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/product/delete/{id}", name= "deleteProduct")
+     *
+     * @param ProductRepository $productRepository
+     * @param integer $id idProduct
+     * @return Response
+     */
+    public function deleteProduct(Product $product, EntityManagerInterface $em, $id): Response
+    {
+        // $product = $productRepository->find($id);
+        $em->remove($product);
+        $em->flush();
+        return $this->redirectToRoute('success');
     }
 }
